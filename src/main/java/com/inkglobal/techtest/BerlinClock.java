3181a93fa72ebcda6f7e356a29602f0679d69720
Indy Lalli <inderjeet.lalli@gmail.com>
2014-04-06 20:22:01 +0100

    upper lamps

--- src/main/java/com/inkglobal/techtest/BerlinClock.java
+++ src/main/java/com/inkglobal/techtest/BerlinClock.java
@@ -1,5 +1,7 @@
 package com.inkglobal.techtest;
 
+import static org.junit.Assert.assertEquals;
+
 public class BerlinClock {
 
 	public static Object asBerlinTime(String normalTime) {
@@ -18,12 +20,41 @@ public class BerlinClock {
 	}
 
 	public static String hoursToUpperLamps(int hours) {
-		// TODO Auto-generated method stub
+		/*
+		The top two rows of lamps are red. These indicate the hours of a day. 
+In the top row there are 4 red lamps. Every lamp represents 5 hours. In the lower row of red lamps every lamp represents 1 hour. 
+So if two lamps of the first row and three of the second row are switched on that indicates 5+5+3=13h or 1 pm.
+		assertEquals("OOOO OOOO", BerlinClock.hoursToUpperLamps(00));
+		assertEquals("RROO RRRO", BerlinClock.hoursToUpperLamps(13));
+		assertEquals("RRRR RRRO", BerlinClock.hoursToUpperLamps(23));
+		assertEquals("RRRR RRRR", BerlinClock.hoursToUpperLamps(24));
+		 */
+		int noOfTopRowLampsOn = hours / 5;
+		int noOfBottomRowLampsOn = hours - 5*noOfTopRowLampsOn;
+		return lightupUpperLamps(noOfTopRowLampsOn) + lightupUpperLamps(noOfBottomRowLampsOn);
+	}
+	
+	public static String lightupUpperLamps(int i) {
+		/*
+		assertEquals("OOOO", BerlinClock.lightupUpperLamps(0));
+		assertEquals("ROOO", BerlinClock.lightupUpperLamps(1));
+		assertEquals("RROO", BerlinClock.lightupUpperLamps(2));
+		assertEquals("RRRO", BerlinClock.lightupUpperLamps(3));
+		assertEquals("RRRR", BerlinClock.lightupUpperLamps(4));
+		 */
 		return null;
 	}
 
 	public static String minutesToLowerLamps(int minutes) {
-		// TODO Auto-generated method stub
+		/*
+		The two rows of lamps at the bottom count the minutes. The first of these rows has 11 lamps, the second 4. 
+In the first row every lamp represents 5 minutes. 
+In this first row the 3rd, 6th and 9th lamp are red and indicate the first quarter, half and last quarter of an hour. 
+The other lamps are yellow. In the last row with 4 lamps every lamp represents 1 minute.
+		assertEquals("OOOOOOOOOOO OOOO", BerlinClock.minutesToLowerLamps(00));
+		assertEquals("YYROOOOOOOO YYOO", BerlinClock.minutesToLowerLamps(17));
+		assertEquals("YYRYYRYYRYY YYYY", BerlinClock.minutesToLowerLamps(59));
+		 */
 		return null;
-	}
+	}	
 }
--- src/test/java/com/inkglobal/techtest/BerlinClockTest.java
+++ src/test/java/com/inkglobal/techtest/BerlinClockTest.java
@@ -6,19 +6,6 @@ import org.junit.Test;
 
 public class BerlinClockTest {
 	
-	/*
-	On the top of the clock there is a yellow lamp that blinks on/off every two seconds. 
-
-The top two rows of lamps are red. These indicate the hours of a day. 
-In the top row there are 4 red lamps. Every lamp represents 5 hours. In the lower row of red lamps every lamp represents 1 hour. 
-So if two lamps of the first row and three of the second row are switched on that indicates 5+5+3=13h or 1 pm.
-
-The two rows of lamps at the bottom count the minutes. The first of these rows has 11 lamps, the second 4. 
-In the first row every lamp represents 5 minutes. 
-In this first row the 3rd, 6th and 9th lamp are red and indicate the first quarter, half and last quarter of an hour. 
-The other lamps are yellow. In the last row with 4 lamps every lamp represents 1 minute.
-	 */
-	
 	@Test
 	public void testAsBerlinTime() {
 		assertEquals("Y OOOO OOOO OOOOOOOOOOO OOOO", BerlinClock.asBerlinTime("00:00:00"));
@@ -40,6 +27,15 @@ The other lamps are yellow. In the last row with 4 lamps every lamp represents 1
 		assertEquals("RROO RRRO", BerlinClock.hoursToUpperLamps(13));
 		assertEquals("RRRR RRRO", BerlinClock.hoursToUpperLamps(23));
 		assertEquals("RRRR RRRR", BerlinClock.hoursToUpperLamps(24));
+	}
+	
+	@Test
+	public void testLightupUpperLamps() {
+		assertEquals("OOOO", BerlinClock.lightupUpperLamps(0));
+		assertEquals("ROOO", BerlinClock.lightupUpperLamps(1));
+		assertEquals("RROO", BerlinClock.lightupUpperLamps(2));
+		assertEquals("RRRO", BerlinClock.lightupUpperLamps(3));
+		assertEquals("RRRR", BerlinClock.lightupUpperLamps(4));
 	}
 	
 	@Test
